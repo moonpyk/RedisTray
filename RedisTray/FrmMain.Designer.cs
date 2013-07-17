c59d9b3cfc2e6e7190e966f3c347eb0864166456
Clément Bourgeois <moonpyk@gmail.com>
2013-07-17 23:52:34 +0200

    Initial commit

---
+++ RedisTray/FrmMain.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace RedisTray
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabRedisServer = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtRedisOut = new System.Windows.Forms.TextBox();
+            this.btnToggleServer = new System.Windows.Forms.CheckBox();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabRedisServer.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "RedisTray";
+            this.notifyIcon.Visible = true;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.tabControl);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(605, 366);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(605, 390);
+            this.toolStripContainer1.TabIndex = 2;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabRedisServer);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(3, 3);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(602, 360);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabRedisServer
+            // 
+            this.tabRedisServer.Controls.Add(this.btnToggleServer);
+            this.tabRedisServer.Controls.Add(this.txtRedisOut);
+            this.tabRedisServer.Location = new System.Drawing.Point(4, 22);
+            this.tabRedisServer.Name = "tabRedisServer";
+            this.tabRedisServer.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRedisServer.Size = new System.Drawing.Size(594, 334);
+            this.tabRedisServer.TabIndex = 0;
+            this.tabRedisServer.Text = "Redis";
+            this.tabRedisServer.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(511, 330);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(605, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // txtRedisOut
+            // 
+            this.txtRedisOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRedisOut.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRedisOut.Location = new System.Drawing.Point(6, 6);
+            this.txtRedisOut.Multiline = true;
+            this.txtRedisOut.Name = "txtRedisOut";
+            this.txtRedisOut.ReadOnly = true;
+            this.txtRedisOut.Size = new System.Drawing.Size(582, 283);
+            this.txtRedisOut.TabIndex = 0;
+            // 
+            // btnToggleServer
+            // 
+            this.btnToggleServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnToggleServer.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnToggleServer.Location = new System.Drawing.Point(3, 295);
+            this.btnToggleServer.Name = "btnToggleServer";
+            this.btnToggleServer.Size = new System.Drawing.Size(588, 36);
+            this.btnToggleServer.TabIndex = 1;
+            this.btnToggleServer.Text = "checkBox1";
+            this.btnToggleServer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnToggleServer.UseVisualStyleBackColor = true;
+            this.btnToggleServer.CheckedChanged += new System.EventHandler(this.btnToggleServer_CheckedChanged);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 390);
+            this.Controls.Add(this.toolStripContainer1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Text = "RedisTray";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabRedisServer.ResumeLayout(false);
+            this.tabRedisServer.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabRedisServer;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtRedisOut;
+        private System.Windows.Forms.CheckBox btnToggleServer;
+    }
+}
+
---
+++ RedisTray/FrmMain.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedisTray
+{
+    public partial class FrmMain : Form
+    {
+        private Process _redisProcess;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleServer_CheckedChanged(object sender, System.EventArgs e)
+        {
+            if (_redisProcess == null)
+            {
+                RedisProcessStart();
+            }
+        }
+
+        private void RedisProcessStart()
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName               = @"C:\Users\moonpyk\Desktop\Dev\Redis\redis-server.exe",
+                UseShellExecute        = false,
+                CreateNoWindow         = true,
+                RedirectStandardError  = true,
+                RedirectStandardInput  = true,
+                RedirectStandardOutput = true,
+                StandardOutputEncoding = Encoding.UTF8,
+                StandardErrorEncoding  = Encoding.UTF8,
+            };
+
+            _redisProcess = Process.Start(processStartInfo);
+
+            _redisProcess.OutputDataReceived += RedisProcessOnOutputDataReceived;
+            _redisProcess.ErrorDataReceived += RedisProcessOnOutputDataReceived;
+
+            _redisProcess.BeginOutputReadLine();
+            _redisProcess.BeginErrorReadLine();
+        }
+
+        private void RedisProcessClose()
+        {
+            if (_redisProcess != null)
+            {
+                _redisProcess.CancelErrorRead();
+                _redisProcess.CancelOutputRead();
+                _redisProcess.Kill();
+                _redisProcess.Dispose();
+            }
+        }
+
+        private void AppendRedisOutText(string t)
+        {
+            Invoke((MethodInvoker)delegate
+            {
+                if (string.IsNullOrWhiteSpace(t))
+                {
+                    return;
+                }
+                txtRedisOut.AppendText(t + Environment.NewLine);
+            });
+        }
+
+        private void RedisProcessOnOutputDataReceived(object sender, DataReceivedEventArgs dataReceivedEventArgs)
+        {
+            AppendRedisOutText(dataReceivedEventArgs.Data);
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RedisProcessClose();
+        }
+    }
+}
